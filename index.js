e89dbaaf70c32e95ffe3f567562d7d0d9f8e5ff6
Tracy Dean Blackadder <tracey.blackadder@realestate.com.au>
2019-09-04 21:17:51 +1000

    robot can turn

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ let robot = {
   y: null,
   f: "",
 }
+
 function place(x, y, f) {
   robot.x = x;
   robot.y = y;
   robot.f = f;
+  console.log(robot.x, robot.y, robot.f) 
 }
-// place(0,1,'south')
-// place(4,1,'north')
-// place(0,4,'east')
-// place(3,0,'west')
 
 function move() {
   (robot.f == 'north') ? robot.x = robot.x + 1 : (robot.f == 'south' ) ? robot.x = robot.x - 1 : (robot.f == 'east' ) ? robot.y = robot.y + 1 : (robot.f == 'west' ) ? robot.y = robot.y - 1 : null
@@ -27,8 +25,40 @@ function move() {
   if (robot.y == -1) {
     robot.y = 0
   }
+  console.log(robot.x, robot.y, robot.f) 
+}
+
+function right() {
+  if (robot.f == 'north') {
+    robot.f = 'east'
+  } else if (robot.f == 'east') {
+    robot.f = 'south'
+  } else if (robot.f == 'south') {
+    robot.f = 'west'
+  } else if (robot.f == 'west') {
+    robot.f = 'north'
+  }
+  console.log(robot.x, robot.y, robot.f) 
+}
+
+let left = () => {
+  if (robot.f == 'north') {
+    robot.f = 'west'
+  } else if (robot.f == 'west') {
+    robot.f = 'south'
+  } else if (robot.f == 'south') {
+    robot.f = 'east'
+  } else if (robot.f == 'east') {
+    robot.f = 'north'
+  }
+  console.log(robot.x, robot.y, robot.f) 
 }
 
 function report() {
   console.log(robot.x, robot.y, robot.f) 
 }
+
+// place(0,1,'south')
+// place(4,1,'north')
+// place(0,4,'east')
+// place(3,0,'west')
