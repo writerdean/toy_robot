fe331846893d9e9a2b0ebbd85abdab1d4277d05a
Tracy Dean Blackadder <tracey.blackadder@realestate.com.au>
2019-09-10 12:11:00 +1000

    add eslint

--- index.js
+++ index.js
@@ -1,75 +1,73 @@
-let robot = {
+const robot = {
   placed: false,
   x: null,
   y: null,
-  f: "",
-}
+  f: '',
+};
 
-function place(x, y, f) {
+
+const place = (x, y, f) => {
   robot.x = x;
   robot.y = y;
   robot.f = f;
-  robot.placed = true
-}
+  robot.placed = true;
+};
 
-function move() {
-  if (robot.placed == false) {
-    return
+const move = () => {
+  if (robot.placed === false) {
+    return;
   }
-  (robot.f == 'north') ? robot.x = robot.x + 1 : (robot.f == 'south' ) ? robot.x = robot.x - 1 : (robot.f == 'east' ) ? robot.y = robot.y + 1 : (robot.f == 'west' ) ? robot.y = robot.y - 1 : null
-  if (robot.x == 5) {
-    robot.x = 4
+  (robot.f === 'north') ? robot.x = robot.x + 1 : (robot.f === 'south') ? robot.x = robot.x - 1 : (robot.f === 'east') ? robot.y = robot.y + 1 : (robot.f === 'west') ? robot.y = robot.y - 1 : null;
+  if (robot.x === 5) {
+    robot.x = 4;
   }
-  if (robot.x == -1) {
-    robot.x = 0
+  if (robot.x === -1) {
+    robot.x = 0;
   }
-  if (robot.y == 5) {
-    robot.y = 4
+  if (robot.y === 5) {
+    robot.y = 4;
   }
-  if (robot.y == -1) {
-    robot.y = 0
+  if (robot.y === -1) {
+    robot.y = 0;
   }
-  console.log(robot.x, robot.y, robot.f) 
-}
+};
 
-function right() {
-  if (robot.placed == false) {
-    return
+const right = () => {
+  if (robot.placed === false) {
+    return;
   }
-  if (robot.f == 'north') {
-    robot.f = 'east'
-  } else if (robot.f == 'east') {
-    robot.f = 'south'
-  } else if (robot.f == 'south') {
-    robot.f = 'west'
-  } else if (robot.f == 'west') {
-    robot.f = 'north'
+  if (robot.f === 'north') {
+    robot.f = 'east';
+  } else if (robot.f === 'east') {
+    robot.f = 'south';
+  } else if (robot.f === 'south') {
+    robot.f = 'west';
+  } else if (robot.f === 'west') {
+    robot.f = 'north';
   }
-  console.log(robot.x, robot.y, robot.f) 
-}
+};
 
-let left = () => {
-  if (robot.placed == false) {
-    return
+const left = () => {
+  if (robot.placed === false) {
+    return;
   }
-  if (robot.f == 'north') {
-    robot.f = 'west'
-  } else if (robot.f == 'west') {
-    robot.f = 'south'
-  } else if (robot.f == 'south') {
-    robot.f = 'east'
-  } else if (robot.f == 'east') {
-    robot.f = 'north'
+  if (robot.f === 'north') {
+    robot.f = 'west';
+  } else if (robot.f === 'west') {
+    robot.f = 'south';
+  } else if (robot.f === 'south') {
+    robot.f = 'east';
+  } else if (robot.f === 'east') {
+    robot.f = 'north';
   }
-  console.log(robot.x, robot.y, robot.f) 
-}
+};
 
-function report() {
-  if (robot.placed == false) {
-    return
+const report = () => {
+  if (robot.placed === false) {
+    return;
   }
-  console.log(robot.x, robot.y, robot.f) 
-}
+  console.log(`${robot.x},${robot.y},${robot.f}`);
+};
 
 // place(0,1,'south')
 // place(4,1,'north')
